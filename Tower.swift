1c2a4315642e860163ac507dae992e41ff812a68
Peterzhu123 <Peter.zhu@ocsbstudent.ca>
2018-12-21 17:47:01 -0500

    Create Tower.swift

---
+++ Tower.swift
@@ -0,0 +1,50 @@
+// Created by: Peter Zhu
+// Created on: Oct
+
+// Recursive Tower of Hanoi program
+ 
+
+import Foundation
+
+func minMoves(num: Int) -> Decimal {
+    // returns minimum number of moves
+    var moves = pow(2, num) - 1
+    return moves
+}
+
+func printMoves(num: Int, startPole: String, endPole: String) {
+    // prints out instructions for move
+    print("Move disk \(num) from \(startPole) to \(endPole).")
+}
+
+func moves(num: Int, startPole: String, endPole: String, auxPole: String) {
+    // calculates best solution for where to move disks and solve problem
+    if(num == 1) {
+        printMoves(num: num, startPole: startPole, endPole: endPole)
+    }
+    else {
+        moves(num: num - 1, startPole: startPole, endPole: auxPole, auxPole: endPole)
+        printMoves(num: num, startPole: startPole, endPole: endPole)
+        moves(num: num - 1, startPole: auxPole, endPole: endPole, auxPole: startPole)
+    }
+}
+
+var disks: Int
+
+let poleA = "Pole A"
+let poleB = "Pole B"
+let poleC = "Pole C"
+
+print("Enter a postive integer for the number of disks: ")
+
+disks = askForNumber()
+
+if disks == 0 {
+    print("Error")
+}
+else {
+    let min = minMoves(num: disks)
+    
+    print("Minimum number of moves: \(min)")
+    moves(num: disks, startPole: poleA, endPole: poleB, auxPole: poleC)
+}
